34e6beb131fa566a9a02152015679a571013853b
gaganchittoria528462 <57328407+gaganchittoria528462@users.noreply.github.com>
2020-08-11 21:32:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ http.listen(port, () => {
     console.log("Listening on port ", port)
 })
 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
 // app.use(express.static(path.join(__dirname, "public")));
 
 const users = {};
