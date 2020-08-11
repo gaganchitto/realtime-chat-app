f3578b8eb773b56999aaa47614f4dd2927b9b285
gaganchittoria528462 <57328407+gaganchittoria528462@users.noreply.github.com>
2020-08-11 21:22:17 +0530

    update

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 //node server
-const port = process.env.port;
-const io = require('socket.io')(port);
+const path = require('path')
+const express = require('express');
+const app = express();
+// const fs = require('fs');
+const http = require('http').createServer(app);
+const io = require("socket.io")(http);
+const port = process.env.port || 80;
+
+
+http.listen(port, () => {
+    console.log("Listening on port ", port)
+})
+
+// app.use(express.static(path.join(__dirname, "public")));
 
 const users = {};
 
